e8def6e361770cd9ab22bd5f6495edc88ea8c802
Weston <weston.cooper@carruslearn.com>
2021-04-06 11:39:19 -0400

    update grpc, protoloader

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const grpc = require("grpc");
+const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 
 function getProtoFromPackageDefinition(packageDefinition, packageName) {
@@ -31,7 +31,7 @@ class GrpcServer {
     this.server.addService(proto[serviceName].service, router);
     return this;
   }
-  
+
   listen(address, creds=grpc.ServerCredentials.createInsecure()){
     this.server.bind(address, creds);
     this.server.start();
